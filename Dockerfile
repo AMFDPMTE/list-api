65e08b06ea3a0bf42ef46ad26e0843084b4bf0ea
Aren Patel <git@arenpatel.com>
2017-09-28 17:51:01 -0700

    Executing query against db.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM golang:1.9-alpine
 
 # Package dependencies
-RUN apk add --update --no-cache git libc-dev
+RUN apk add --update --no-cache git libc-dev gcc
 
 # install dependency tool
 RUN go get github.com/golang/dep && go install github.com/golang/dep/cmd/dep
--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/AMFDPMTE/list"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	l := list.New()
 	fmt.Println(l.Serialize())
+
+	// sqlite time!
+	db, err := sql.Open("sqlite3", "./db/db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+
+	// select
+	stmt, err := db.Prepare("SELECT * FROM lists")
+	if err != nil {
+		panic(err)
+	}
+	res, err := stmt.Exec()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res)
 }
